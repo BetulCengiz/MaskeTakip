9345e003510a9ba592c4684cabecc6bf384b1ec9
BetulCengiz <bc199911@hotmail.com>
2024-04-03 21:46:49 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class PttManager :ISupplierService
+    {
+        private IApplicantService _applicantService; //dependency
+        private ForeignerManager foreignerManager;
+
+        public PttManager(IApplicantService applicantService) //constructor
+        { 
+        
+        _applicantService = applicantService;
+    
+        }
+
+        public PttManager(ForeignerManager foreignerManager)
+        {
+            this.foreignerManager = foreignerManager;
+        }
+
+        public void GiveMask(PersonManager person) //operasyon
+        {
+            if (_applicantService.CheckPerson(person) )
+            {
+
+                Console.WriteLine(person.FirstName + " için maske verildi.jjjj");
+            }
+
+
+        }
+
+        public void GiveMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using Entities.Concrete;
+using System.Runtime.CompilerServices;
+
+
+SelamVer("Betül");
+
+static void SelamVer(string isim ="isimsiz")
+{
+    Console.WriteLine("Merhaba "+isim);
+}
+Person person1 = new Person();
+person1.FirstName = "Betül";
+person1.LastName = "Cengiz";
+person1.NationalIdentity = 10519819970;
+person1.DateOfBirthYear = 1999;
+
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person1);
